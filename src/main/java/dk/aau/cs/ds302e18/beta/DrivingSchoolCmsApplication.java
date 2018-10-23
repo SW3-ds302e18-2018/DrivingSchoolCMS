5df8328aea04d455f9a08920a9fea8d49721207d
Peter K <pkjar16@student.aau.dk>
2018-10-23 09:59:04 +0200

    Initial Commit

---
+++ src/main/java/dk/aau/cs/ds302e18/beta/DrivingSchoolCmsApplication.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.ds302e18.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolCmsApplication.class, args);
+	}
+}
---
+++ src/test/java/dk/aau/cs/ds302e18/beta/DrivingSchoolCmsApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.aau.cs.ds302e18.beta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrivingSchoolCmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
