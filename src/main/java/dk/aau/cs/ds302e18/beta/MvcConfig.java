c81f79be321690a30100a56649627531a8e0f755
Unknown <seraph14@hotmail.com>
2018-10-26 12:03:07 +0200

    Removed useless files renamed a package

--- src/main/java/dk/aau/cs/ds302e18/beta/MvcConfig.java
+++ src/main/java/dk/aau/cs/ds302e18/beta/MvcConfig.java
@@ -1,4 +1,4 @@
-package dk.aau.cs.ds302e18.beta.config;
+package dk.aau.cs.ds302e18.beta;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
